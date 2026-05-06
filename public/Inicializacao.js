be10d5aac6469a4a1a79942151b59996ec838ecf
SadMan <teteusg109@gmail.com>
2026-05-06 11:05:34 -0300

    teste 8

--- public/Inicializacao.js
+++ public/Inicializacao.js
@@ -63,12 +63,7 @@ async function init() {
 
   localStorage.setItem('dxp2_lastopen', today);
   renderMissions();
-  state.categories = state.categories.map(c => ({
-    ...c,
-    icon:  c.icon  || 'fa-solid fa-circle',
-    color: c.color || '#7C6FCD',
-  }));
-  save();
+  
 }
 
 async function resumePendingVoiceJob() {
